920fae6b4f53ed6ce723e26c8d2f1a382b2df2b3
Guilherme Correa <78979736+guilhermedcorrea@users.noreply.github.com>
2023-05-16 07:13:49 -0300

    app
    app

--- getjobs/getjobs/pipelines.py
+++ getjobs/getjobs/pipelines.py
@@ -1,13 +1,48 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
-
-# useful for handling different item types with a single interface
+from sqlalchemy import create_engine
+from sqlalchemy.engine import URL
+import os
+from pathlib import Path
+from urllib import parse
+from dotenv import load_dotenv
+from os import path
 from itemadapter import ItemAdapter
+
+user_mssql = os.getenv('serveruser')
+password_mssql = os.getenv('serverpassword')
+database_mssql = os.getenv('serverdatabase')
+host_mssql = os.getenv('serverhost')
+
+
+def mssq_datawharehouse():
+    connection_url = URL.create(
+            "mssql+pyodbc",
+            username=f"{user_mssql}",
+            password=f"{password_mssql}",
+            host=f"{host_mssql}",
+            database=f"{database_mssql}",
+            query={
+                "driver": "ODBC Driver 17 for SQL Server",
+                "autocommit": "True",
+        }
+        )
+      
+    engine = create_engine(connection_url).execution_options(
+    isolation_level="AUTOCOMMIT", future=True,fast_executemany=True)
+    return engine
 
 
 class GetjobsPipeline:
     def process_item(self, item, spider):
         return item
+    
+    def authentication(self, *args, **kwargs):
+        ...
+    
+    def inserts(self, *args, **kwargs):
+        ...
+        
+    def update(self, *args, **kwargs):
+        ...
+        
+    def delete(self, *args, **kwargs):
+        ...
